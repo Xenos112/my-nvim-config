21586774a6949c74930fcb24792ac950e1889e08
Xenos <acristyano22@gmail.com>
2025-01-04 09:11:32 -0800

    add two keymaps to let the cursor in the center when moving using `CTRL+u/d`

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -99,8 +99,8 @@ return {
     opts = {
       auto_update         = true,
       neovim_image_text   = "The One True Text Editor",
-      main_image          = "",
-      client_id           = "",
+      main_image          = "6999296",
+      client_id           = "1325135620707713035",
       log_level           = 'info',
       debounce_timeout    = 10,
       enable_line_number  = false,
