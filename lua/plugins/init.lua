be645c4c640152b1ad4641f1b4fd5341b0d90eb8
Xenos <acristyano22@gmail.com>
2025-01-04 09:06:27 -0800

    add Discord Presence plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -92,5 +92,29 @@ return {
     "ibhagwan/fzf-lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {}
-  }
+  },
+  {
+    'andweeb/presence.nvim',
+    lazy = false,
+    opts = {
+      auto_update         = true,
+      neovim_image_text   = "The One True Text Editor",
+      main_image          = "",
+      client_id           = "",
+      log_level           = 'info',
+      debounce_timeout    = 10,
+      enable_line_number  = false,
+      blacklist           = {},
+      buttons             = true,
+      file_assets         = {},
+      show_time           = true,
+      editing_text        = "Flexing on Others in %s",
+      file_explorer_text  = "Flexing While Exploring",
+      git_commit_text     = "Flexing while committing changes",
+      plugin_manager_text = "Flexing while Managing plugins",
+      reading_text        = "Reading and Flexing in %s",
+      workspace_text      = "Flexing on YEAHHHH",
+      line_number_text    = "Line %s out of %s",
+    }
+  },
 }
