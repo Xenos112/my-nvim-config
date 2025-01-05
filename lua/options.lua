22fccbf6a9e70d6aebece01384b6815c59d02d14
Xenos <acristyano22@gmail.com>
2025-01-05 09:46:27 -0800

    add a scroll off with value of 8 to make vertical space when scrolling in a long file

--- lua/options.lua
+++ lua/options.lua
@@ -1,9 +1,5 @@
 require "nvchad.options"
 
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
 vim.opt.relativenumber = true
 
 vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#eb6f92", fg = "#191724" })
@@ -15,3 +11,5 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     })
   end,
 })
+
+vim.opt.scrolloff = 8
