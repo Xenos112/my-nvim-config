d3305bd4a6e99584117a98490e30b58f89c77b39
Xenos <acristyano22@gmail.com>
2025-01-05 09:41:25 -0800

    make it when i yank something it will flash a red background for 200ms

--- lua/options.lua
+++ lua/options.lua
@@ -5,3 +5,13 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 vim.opt.relativenumber = true
+
+vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#eb6f92", fg = "#191724" })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "YankHighlight",
+      timeout = 200,
+    })
+  end,
+})
