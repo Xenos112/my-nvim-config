f65e84df86015ad4af03c628bcf83fb9050010f2
Xenos <acristyano22@gmail.com>
2025-01-15 08:45:45 -0800

    add desc for the hop plugin

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,14 +1,13 @@
 return {
-	"smoka7/hop.nvim",
-	event = "BufRead",
-	version = "*",
-	opts = {
-		keys = "etovxqpdygfblzhckisuran",
-	},
-	config = function()
-		require("hop").setup({})
+  "smoka7/hop.nvim",
+  event = "BufRead",
+  version = "*",
+  opts = {
+    keys = "etovxqpdygfblzhckisuran",
+  },
+  config = function()
+    require("hop").setup({})
 
-		vim.keymap.set("n", "<CR>", ":HopWord<CR>", {})
-		-- HACK: maybe add some of the other mappings
-	end,
+    vim.keymap.set("n", "<CR>", ":HopWord<CR>", { desc = "Hop to word under cursor", silent = true })
+  end,
 }
