55890d6f0279dae51a26b3ef367f20a54f266b05
Xenos <acristyano22@gmail.com>
2025-01-18 03:41:56 -0800

    add `oil.nvim` as main file explorer

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,46 +1,57 @@
 return {
-{
-	"lewis6991/gitsigns.nvim",
-	event = "BufRead",
-	config = function()
-		require("gitsigns").setup({})
-	end,
-},
-{
-  "andweeb/presence.nvim",
-  lazy = false,
-  opts = {
-    auto_update = true,
-    neovim_image_text = "The One True Text Editor",
-    main_image = "6999296",
-    client_id = "1325135620707713035",
-    log_level = nil,
-    debounce_timeout = 10,
-    enable_line_number = true,
-    blacklist = {},
-    buttons = true,
-    file_assets = {},
-    show_time = true,
-    editing_text = "Flexing on Others in %s",
-    file_explorer_text = "Flexing While Exploring",
-    git_commit_text = "Flexing while committing changes",
-    plugin_manager_text = "Flexing while Managing plugins",
-    reading_text = "Reading and Flexing in %s",
-    workspace_text = "Flexing on YEAHHHH",
-    line_number_text = "Line %s out of %s",
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufRead",
+    config = function()
+      require("gitsigns").setup({})
+    end,
   },
-},
-{
-  "smoka7/hop.nvim",
-  event = "BufRead",
-  version = "*",
-  opts = {
-    keys = "etovxqpdygfblzhckisuran",
+  {
+    "andweeb/presence.nvim",
+    lazy = false,
+    opts = {
+      auto_update = true,
+      neovim_image_text = "The One True Text Editor",
+      main_image = "6999296",
+      client_id = "1325135620707713035",
+      log_level = nil,
+      debounce_timeout = 10,
+      enable_line_number = true,
+      blacklist = {},
+      buttons = true,
+      file_assets = {},
+      show_time = true,
+      editing_text = "Flexing on Others in %s",
+      file_explorer_text = "Flexing While Exploring",
+      git_commit_text = "Flexing while committing changes",
+      plugin_manager_text = "Flexing while Managing plugins",
+      reading_text = "Reading and Flexing in %s",
+      workspace_text = "Flexing on YEAHHHH",
+      line_number_text = "Line %s out of %s",
+    },
   },
-  config = function()
-    require("hop").setup({})
+  {
+    "smoka7/hop.nvim",
+    event = "BufRead",
+    version = "*",
+    opts = {
+      keys = "etovxqpdygfblzhckisuran",
+    },
+    config = function()
+      require("hop").setup({})
 
-    vim.keymap.set("n", "<CR>", ":HopWord<CR>", { desc = "Hop to word under cursor", silent = true })
-  end,
-}
+      vim.keymap.set("n", "<CR>", ":HopWord<CR>", { desc = "Hop to word under cursor", silent = true })
+    end,
+  },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    config = function()
+      require("oil").setup({})
+      vim.keymap.set("n", "<leader>e", ":Oil<CR>", { desc = "Open Oil", silent = true })
+    end,
+  }
 }
