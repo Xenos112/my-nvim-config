60da3463b858002a50ff8034ea3fb1bfd26d2f32
Xenos112 <acristyano22@gmail.com>
2025-01-19 04:35:49 -0800

    make the todo plugin use telescope instead of normal quickfix menu

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -30,7 +30,7 @@ return {
     event = "BufReadPost",
     config = function()
       require("todo-comments").setup({})
-      vim.keymap.set("n", "<leader>td", ":TodoQuickFix<CR>", { desc = "Todo QuickFix" })
+      vim.keymap.set("n", "<leader>td", ":TodoTelescope<CR>", { desc = "Todo QuickFix" })
     end,
   },
   {
