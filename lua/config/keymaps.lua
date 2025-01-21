4075539338259490cdd56faf3e3085014600f8db
Abdelmadjid <acristyano22@gmail.com>
2025-01-21 15:28:11 -0800

    make the tab buffer navigation in silent mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,8 +7,8 @@ map("v", "<leader>/", "gc", {})
 map("n", "<leader>?", "g?", {})
 map("n", "<leader>/", "gcc", {})
 
-map("n", "<tab>", ":bn<CR>", {})
-map("n", "<S-tab>", ":bp<CR>", {})
+map("n", "<tab>", ":bn<CR>", { silent = true })
+map("n", "<S-tab>", ":bp<CR>", { silent = true })
 
 map("n", "<leader>x", ":bd<CR>", {})
 map("v", "<A-j>", ":m '>+1<CR>gv=gv", {})
