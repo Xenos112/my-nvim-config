c2e2993b34970075cc7ae53ecf1ba0d445b11654
Xenos112 <acristyano22@gmail.com>
2025-01-28 19:17:37 -0800

    make the bg black instead of the default rose pine theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -160,6 +160,7 @@ return {
 		config = function()
 			vim.cmd("colorscheme rose-pine")
 			vim.cmd("hi Comment gui=italic")
+			vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
 			vim.cmd("hi String gui=italic")
 			vim.cmd("hi Keyword gui=italic")
 		end,
